3e1c2f6aec7f1995bd94c6bf635883802629115d
Yogek Garg <yogekgarg.dev@gmail.com>
2022-08-12 12:36:47 +0530

    Time: 11 ms (70.01%), Space: 71.7 MB (18.05%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
+        //-> Improved Solution 
+        // Rather than checking for each and every elements .contains() use 'boolean add()' only
+        // above function return false, if element already present in the hashset.
+        
         HashSet<Integer> hset=new HashSet<>();
         
         for(int i=0;i<nums.length;i++)
         {
-            if(hset.contains(nums[i]))
+            if(!hset.add(nums[i]))
             {
                 return true;
             }
-            hset.add(nums[i]);
-            
         }
-        
         return false;
+        
         
     }
 }
